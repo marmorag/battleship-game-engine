ce051c9018cff3b3c8d04a11848a8a1c061a09c2
marmorag <g.marmo@hotmail.fr>
2019-09-25 22:56:28 +0200

    fix jest problem

--- src/tests/Game.test.ts
+++ src/tests/Game.test.ts
@@ -1,9 +1,9 @@
-import Game from "../src/Game";
+import { Game } from "../core/Game";
 
 describe('Behavior', () => {
     it('should be able to initialize a new game', function () {
         const game = new Game();
 
-        expect(game).toHaveProperty('grid');
+        expect(game.hasStarted).toBe(false);
     });
 });
