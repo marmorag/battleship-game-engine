d096d32a5ce0f12a54c96f2278eee777a05cd061
marmorag <g.marmo@hotmail.fr>
2019-09-25 22:57:10 +0200

    utils

---
+++ src/utils/RandomPicker.ts
@@ -0,0 +1,13 @@
+
+export class RandomPicker {
+
+    static randomFromEnum<T>(anEnum: T): T[keyof T] {
+        const enumValues = Object.keys(anEnum)
+            .map(n => Number.parseInt(n))
+            .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][];
+
+        const randomIndex = Math.floor(Math.random() * enumValues.length);
+
+        return enumValues[randomIndex];
+    }
+}
