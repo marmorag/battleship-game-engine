a45cd169f520c8968e497ab89c2828ebc3d773e9
marmorag <g.marmo@hotmail.fr>
2019-09-27 23:01:26 +0200

    package json update

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,22 @@ export * from "./core/Player";
 export * from "./core/Grid";
 export * from "./core/Game";
 export * from "./core/Warship";
+
+export * from "./core/warships/Carrier";
+export * from "./core/warships/Destroyer";
+export * from "./core/warships/Cruiser";
+export * from "./core/warships/Frigate";
+
 export * from "./utils/Coordinate";
+export * from "./utils/GameConfig";
+export * from "./utils/GameResult";
+export * from "./utils/GameStatsTracker";
+
 export * from "./exception/InvalidOrientationException";
 export * from "./exception/WarshipNotPlacedException";
 export * from "./exception/WarshipAlreadyPlacedException";
+export * from "./exception/InvalidPlayerException";
+export * from "./exception/InvalidPlacementException";
+export * from "./exception/InvalidGameStatusException";
 
 export default Game;
-export {GameConfig} from "./utils/GameConfig";
-export {InvalidPlayerException} from "./exception/InvalidPlayerException";
-export {InvalidPlacementException} from "./exception/InvalidPlacementException";
-export {InvalidGameStatusException} from "./exception/InvalidGameStatusException";
-export {Carrier} from "./core/warships/Carrier";
-export {Destroyer} from "./core/warships/Destroyer";
-export {Cruiser} from "./core/warships/Cruiser";
-export {Frigate} from "./core/warships/Frigate";
