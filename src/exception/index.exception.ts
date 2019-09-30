b87da0295c91a58cf362b280bbbe3b670ba77c1d
marmorag <g.marmo@hotmail.fr>
2019-10-01 00:28:20 +0200

    re organized index files

---
+++ src/exception/index.exception.ts
@@ -0,0 +1,6 @@
+export * from "./InvalidOrientationException";
+export * from "./WarshipNotPlacedException";
+export * from "./WarshipAlreadyPlacedException";
+export * from "./InvalidPlayerException";
+export * from "./InvalidPlacementException";
+export * from "./InvalidGameStatusException";
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,7 @@
 import {Game} from "./core/Game";
 
-export * from "./core/Player";
-export * from "./core/Grid";
-export * from "./core/Game";
-export * from "./core/Warship";
-
-export * from "./core/warships/Carrier";
-export * from "./core/warships/Destroyer";
-export * from "./core/warships/Cruiser";
-export * from "./core/warships/Frigate";
-
-export * from "./utils/Coordinate";
-export * from "./utils/GameConfig";
-export * from "./utils/GameResult";
-export * from "./utils/stats/GameStatsTracker";
-
-export * from "./exception/InvalidOrientationException";
-export * from "./exception/WarshipNotPlacedException";
-export * from "./exception/WarshipAlreadyPlacedException";
-export * from "./exception/InvalidPlayerException";
-export * from "./exception/InvalidPlacementException";
-export * from "./exception/InvalidGameStatusException";
+export * from "./core/index.core";
+export * from "./utils/index.utils";
+export * from "./exception/index.exception";
 
 export default Game;
-export {Team} from "./utils/enum/Team";
-export {ShotStatus} from "./utils/enum/ShotStatus";
-export {WarshipClass} from "./utils/enum/WarshipClass";
-export {WarshipPartStatus} from "./utils/enum/WarshipPartStatus";
-export {WarshipPlacementStatus} from "./utils/enum/WarshipPlacementStatus";
